7fdc2a6237998a9a03dc7c6f127167e2ac557dbb
Crina <varlan.crina@yahoo.com>
2017-06-19 21:55:43 +0100

    social nav/rent page

--- js/script.js
+++ js/script.js
@@ -94,3 +94,17 @@ function producePrompt(message, promptLocation, color) {
   document.getElementById(promptLocation).innerHTML = message;
   document.getElementById(promptLocation).style.color = color;
 }
+/* popup video in games*/
+$(document).ready(function() {
+                  $('.popup-with-zoom-anim').magnificPopup({
+                    type: 'inline',
+                    fixedContentPos: false,
+                    fixedBgPos: true,
+                    overflowY: 'auto',
+                    closeBtnInside: true,
+                    preloader: false,
+                    midClick: true,
+                    removalDelay: 300,
+                    mainClass: 'my-mfp-zoom-in'
+                  });
+                  });
